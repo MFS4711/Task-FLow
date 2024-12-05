d8ee072d4c6da50b32cbb51d72d2aff7cac31475
Muhammad Shabir <n48jomzr@students.codeinstitute.net>
2024-12-05 10:35:45 +0000

    install django all uth and amend settings.py to account for this - also add path to urls.py in main project file - template modifications outstanding

--- task_flow/settings.py
+++ task_flow/settings.py
@@ -43,9 +43,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'django_summernote',
     'task_dashboard',
 ]
+
+SITE_ID = 1
+# Change redirect url later to load dashbord when logged in
+LOGIN_REDIRECT_URL = '/' 
+LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -56,6 +65,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'task_flow.urls'
@@ -115,6 +125,9 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+# This ensure no email verification required - maybe change for project
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 
 # Internationalization
